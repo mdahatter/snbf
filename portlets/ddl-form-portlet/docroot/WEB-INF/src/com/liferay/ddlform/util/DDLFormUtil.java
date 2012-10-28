58f90c3a8bea23be8de1635c1edacfe039b11a01
oalopez <oalopez@gmail.com>
2012-10-28 04:11:21 +0000

    Versión funcional portlet ddlform

--- portlets/ddl-form-portlet/docroot/WEB-INF/src/com/liferay/ddlform/util/DDLFormUtil.java
+++ portlets/ddl-form-portlet/docroot/WEB-INF/src/com/liferay/ddlform/util/DDLFormUtil.java
@@ -14,11 +14,14 @@
 
 package com.liferay.ddlform.util;
 
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.service.UserLocalServiceUtil;
 import com.liferay.portal.theme.ThemeDisplay;
 import com.liferay.portal.util.PortalUtil;
 import com.liferay.portlet.dynamicdatalists.model.DDLRecord;
+import com.liferay.portlet.dynamicdatalists.model.DDLRecordSet;
 import com.liferay.portlet.dynamicdatalists.service.DDLRecordLocalServiceUtil;
 
 import java.util.List;
@@ -51,12 +54,34 @@ public class DDLFormUtil {
 
 		List<DDLRecord> records = DDLRecordLocalServiceUtil.getRecords(
 			recordSetId, themeDisplay.getUserId());
-
+		
 		if (records.isEmpty()) {
 			return false;
 		}
 
 		return true;
 	}
-
+	
+	public static boolean hasSubmittedSurvey(
+			HttpServletRequest request, List<DDLRecordSet> newListaDDL)
+		throws SystemException {
+		
+		ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(
+				WebKeys.THEME_DISPLAY);
+		
+		boolean submitted = false;
+		for (int i = 0; i < newListaDDL.size() && !submitted; i++)
+		{
+			
+			List<DDLRecord> records = DDLRecordLocalServiceUtil.getRecords(
+					newListaDDL.get(i).getRecordSetId(), themeDisplay.getUserId());
+			
+			if (!records.isEmpty()) {
+				submitted = true;
+			}
+		}
+		
+		return submitted; 
+		
+	}
 }
---
+++ portlets/ddl-form-portlet/docroot/WEB-INF/src/com/liferay/ddlform/util/DDLSurveyConstants.java
@@ -0,0 +1,17 @@
+package com.liferay.ddlform.util;
+
+public class DDLSurveyConstants {
+
+	public static final String NINOS = "Ninos";
+	public static final String ADOLESCENTES = "Adolescentes";
+	public static final String JOVENES = "Jovenes";
+	
+	public static final int NINOS_EDAD_MIN = 0;
+	public static final int ADOLESCENTES_EDAD_MIN = 12;
+	public static final int JOVENES_EDAD_MIN = 18;
+
+	public static final int NINOS_EDAD_MAX = 11;
+	public static final int ADOLESCENTES_EDAD_MAX = 17;
+	public static final int JOVENES_EDAD_MAX = 999;
+	
+}
